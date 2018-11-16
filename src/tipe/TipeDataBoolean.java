75a7056d5cb56dc3ab4bf4d069bc7737f9b93501
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-11-16 14:24:58 +0700

    bikin tipe data boolean

---
+++ src/tipe/TipeDataBoolean.java
@@ -0,0 +1,11 @@
+package tipe;
+
+public class TipeDataBoolean{
+
+    public static void main(String[] args){
+        Boolean isValid = true;
+        System.out.println(
+            "Nilai dari variable isValid adalah " +  (isValid ? "Benar" : "Salah")
+        );
+    }
+}
