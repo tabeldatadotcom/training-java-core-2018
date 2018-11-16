75e0867e8b1262291b4e21f4149ffb13ddd0ef04
dewissofiati <dewissofiati@gmail.com>
2018-11-16 15:40:51 +0700

    add folder percabangan

---
+++ src/percabangan/SeleksiSwitch.java
@@ -0,0 +1,35 @@
+package percabangan;
+
+public class SeleksiSwitch {
+    public static void main(String[] args) {
+        int bil = 20;
+        switch (bil) {
+        case 10:
+            System.out.println("Nilai sama dengan 10");
+            break;
+        case 20:
+            System.out.println("Nilai sama dengan 20");
+            break;
+        default:
+            System.out.println("Nilai tidak ada yang sama");
+        }
+
+        int ip = 3;
+        switch (ip) {
+        case 4:
+            System.out.println("A");
+            break;
+        case 3:
+            System.out.println("B");
+            // break;
+        case 2:
+            System.out.println("C");
+            // break;
+        case 1:
+            System.out.println("D");
+            break;
+        default:
+            System.out.println("E");
+        }
+    }
+}
